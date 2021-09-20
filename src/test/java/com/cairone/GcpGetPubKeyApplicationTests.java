56050926124cad37141956c47a30b2d71b77b1cf
Diego Cairone <diegocairone@gmail.com>
2021-09-20 17:54:35 -0300

    Initial commit

---
+++ src/test/java/com/cairone/GcpGetPubKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cairone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpGetPubKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
